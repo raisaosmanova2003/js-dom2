cf24b30652576cff3a983ec53624a3d1ac1b43d0
raisaosmanova2003 <tu4F7PRWU@code.edu.az>
2024-04-18 12:04:52 +0400

    js-dom2

--- script.js
+++ script.js
@@ -1,62 +1,66 @@
-const form=document.getElementById("registerForm")
-const name=document.getElementById("name")
-const surName=document.getElementById("surname")
-const age=document.getElementById("age")
-const nationality=document.getElementById("nationality")
-const position=document.getElementById("position")
-const experience=document.getElementById("experience")
-const list=document.getElementById("list")
-const tbody=document.getElementById("formTable")
-const users=[]
+const form = document.getElementById("registerForm");
+const name = document.getElementById("name");
+const surName = document.getElementById("surname");
+const age = document.getElementById("age");
+const nationality = document.getElementById("nationality");
+const position = document.getElementById("position");
+const experience = document.getElementById("experience");
+const list = document.getElementById("list");
+const tbody = document.getElementById("formTable");
+const users = [];
+let counter = 0;
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let newUser = {
+    id: counter,
+    name: name.value,
+    surname: surName.value,
+    age: age.value,
+    nationality: nationality.value,
+    position: position.value,
+    experience: experience.value,
+    };
+    counter++;
+    users.push(newUser);
+    renderUi(users);
+    console.log(users);
+});
 
-form.addEventListener('submit', (e)=> {
-    e.preventDefault()
-   let newUser={
-    name:name.value,
-    surname:surName.value,
-    age:age.value,
-    nationality:nationality.value,
-    position:position.value,
-    experience:experience.value,
-   
-   }
-   
-   users.push(newUser)
-   renderUi(users)
-   console.log(users);
-   
-})
-
-const renderUi = (items)=> {
-    let innerHTML= ""
+const renderUi = (items) => {
+    let innerHTML = "";
     for (let i = 0; i < items.length; i++) {
-        innerHTML+=
-        `<table class="table table-dark">  
-    <tbody>
-      <tr>
-        <th scope="row">${[i]}</th>
+    innerHTML += `
+        <tr>
+        <th scope="row">${items[i].id}</th>
         <td>${items[i].name}</td>
         <td>${items[i].surname}</td>
         <td>${items[i].age}</td>
         <td>${items[i].nationality}</td>
         <td>${items[i].position}</td>
         <td>${items[i].experience}</td>
-        <td><button type='button' onclick='editHandler' class='btn btn-primary'>Edit</button></td>
-        <td><button type='button' onclick='deleteHandler(${users[i].id}' class='btn btn-danger'>Delete</button></td>
-      </tr>
-   
-    </tbody>
-  </table>`
-    }
-    
-    list.innerHTML=innerHTML
+        <td><button type='button' onclick='editHandler(${items[i].id})' class='btn btn-primary'>Edit</button></td>
+        <td><button type='button' onclick='deleteHandler(${items[i].id})' class='btn btn-danger'>Delete</button></td>
+        </tr>`;
 }
-    
-function deleteHandler  (id)  {
-    let target=users.find ((x) => x.id==id) 
-    let indexOfTarget =users.indexOf(target)
-    users.splice(indexOfTarget,1)
+
+tbody.innerHTML = innerHTML;
+};
+
+function deleteHandler(id) {
+    let target = users.find((x) => x.id == id);
+    let indexOfTarget = users.indexOf(target);
+    users.splice(indexOfTarget, 1);
+    renderUi(users,target,indexOfTarget);
+}
+function editHandler(id) {
+    let idloc=users.find(x => x.id == id)
+    let newname=prompt("New name")
+    let newsurname=prompt("New surname")
+    let newage=prompt("New age")
+    let newexperience=prompt("New experience")
+    idloc.name=newname
+    idloc.surname=newsurname
+    idloc.age=newage
+    idloc.experience=newexperience
     renderUi(users)
 }
-
-
